c3f63b313dc927698ddb1c357d3289af60502738
LCS <leechunsiang0724@gmail.com>
2025-11-29 01:33:36 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Hero } from "./components/Hero";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <Header />
       <Hero />
     </div>
   );
